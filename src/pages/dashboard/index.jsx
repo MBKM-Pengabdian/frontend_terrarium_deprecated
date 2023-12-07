5d1952b16d0afb23819ce5ff09254d4e02c57c05
mhdusop <mhduusop@gmail.com>
2023-12-07 23:54:32 +0700

    refactor: remove file dashboard.css

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,5 +1,3 @@
-import "./dashboard.css"
-import "./plugins.bundle.css"
 import { Sidebar } from "./components/sidebar/sidebar"
 // import { Main } from "./components/main/Main"
 // import { Footer } from "./components/footer/Footer"
